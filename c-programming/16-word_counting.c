658c595f87ed21e77e11b27a5fb9f2f760fb270c
wilmitch <mwangimwilfred@gmail.com>
2024-11-24 00:32:12 +0300

    My code practice for c programming

---
+++ c-programming/16-word_counting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/***
+ * Write a program that prints its input one word per line.
+ */
+int main(void)
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+            putchar('\n');
+        else
+            putchar(c);
+    }
+    return (0);
+}
