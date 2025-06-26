aa42aaad811c918adedf6cf1c5e1b36653d6e5d2
wilmitch <mwangimwilfred@gmail.com>
2025-06-26 20:27:23 +0300

    Betty style code

---
+++ c-programming/0-betty_code_style.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * sum_of_function - calculates the sum of given integers
+ * @var_name: define the variable
+ * @a: any other variable and their definition
+ * Description: Longer description of the function
+ * return: 0 always on success
+ */
+int main(void)
+{
+    printf("Code written the betty-style way\n");
+    return (0);
+}
