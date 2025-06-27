b3a95c762995786e7bbda07087d5f68cc1334db4
wilmitch <mwangimwilfred@gmail.com>
2025-06-27 15:36:23 +0300

    updated quotes on file names for 0-preprocessor, 3-name and finalized hello puts

---
+++ c-programming/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 always on success
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
